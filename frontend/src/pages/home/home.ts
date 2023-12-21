869f95c6d824e4a78b72a7b86304d4d33e82972e
SonnyFishback <sonny.fishback7@gmail.com>
2023-12-20 21:01:32 -0500

    connected backend to frontend. getting cors error

--- frontend/src/pages/home/home.ts
+++ frontend/src/pages/home/home.ts
@@ -1,6 +1,25 @@
 import './home.scss';
 
-export const init = () => {
+const products = async () => {
+    try {
+        const url = 'https://w4fduc83wb.execute-api.us-east-1.amazonaws.com/';
+        const options = {
+
+        }
+
+        const response = await fetch(url, options);
+
+    
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const init = async () => {
     // window.location.href = '/home';
     document.title = 'Homepage | E-Commerce'
+
+    // get products
+   const data = await products()
 };
