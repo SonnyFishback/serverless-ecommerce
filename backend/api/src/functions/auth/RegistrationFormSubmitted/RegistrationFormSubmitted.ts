af6fc1665d91633cb922ff6a16ae950ecb5e2f18
SonnyFishback <sonny.fishback7@gmail.com>
2023-12-22 22:55:57 -0500

    created registration lambda placeholder

---
+++ backend/api/src/functions/auth/RegistrationFormSubmitted/RegistrationFormSubmitted.ts
@@ -0,0 +1,18 @@
+export const handler = async (event) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+         products: [{
+          id: 1,
+          name: "Product 1",
+          price: 100,
+          description: "Product 1 description"
+         }]
+        },
+        null,
+        2
+      ),
+    };
+  };
+  
