31b6fef50fd17d19496f6b560abf3448e89113dd
SonnyFishback <sonny.fishback7@gmail.com>
2023-12-24 18:58:14 -0500

    updated vite proxy config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: " https://fuuwh43fi6.execute-api.us-east-1.amazonaws.com/",
+        target: " https://6o5vn4hnd1.execute-api.us-east-1.amazonaws.com",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
